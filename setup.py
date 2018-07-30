8b760c4a628c4fac8e36a595a6967d6a50bce5af
shuhov <shuhovivan@yandex.ru>
2018-07-30 11:56:55 +0300

    Fix name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = "Shuhov Ivan"
 __version__ = "0.0.1"
 
 setup(
-    name="local-helpers-lib",
+    name="local_helpers",
     version=__version__,
     packages=find_packages(),
     install_requires=[],
